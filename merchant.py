d6febf7fccf5ab0fd326b32cf764d2a0a0399524
timj <kkna9877@gmail.com>
2018-09-11 13:46:20 +0100

    Finished project funding, now debugging
    /usr/bin/python3.6 /home/timj/PycharmProjects/MerchantNetwork/sim.py
    Project id:    1, Index :   0,  k:  3.98, theta:  1.73, Payoff:  6.93, Owner:    0,
     Funded: False
    
    Project id:    2, Index :   1,  k:  2.63, theta:  1.94, Payoff:  1.61, Owner:    1,
     Funded: False
    
    Project id:    3, Index :   2,  k:  2.77, theta:  0.53, Payoff:  1.04, Owner:    2,
     Funded: False
    
    Project 2 self funded by 2
    Project id:    4, Index :   3,  k:  2.96, theta:  1.93, Payoff:  2.68, Owner:    3,
     Funded: False
    
    Project id:    5, Index :   4,  k:  2.86, theta:  1.12, Payoff:  1.86, Owner:    4,
     Funded: False
    
    Project id:    6, Index :   5,  k:  1.46, theta:  1.38, Payoff:  4.56, Owner:    5,
     Funded: False
    
    Project 5 self funded by 5
    Project id:    7, Index :   6,  k: 0.997, theta:  0.75, Payoff:  0.73, Owner:    6,
     Funded: False
    
    Project 6 self funded by 6
    Project id:    8, Index :   7,  k:  1.84, theta:  1.23, Payoff:  2.72, Owner:    7,
     Funded: False
    
    Project 7 self funded by 7
    Project id:    9, Index :   8,  k:  2.37, theta:  1.04, Payoff:  0.95, Owner:    8,
     Funded: False
    
    Project 8 self funded by 8
    Project id:   10, Index :   9,  k:  3.09, theta:  1.46, Payoff:  5.66, Owner:    9,
     Funded: False
    
    Problem funding project 9: Sum of investments = 4.38, cost = 4.5
    Project id:    1, Index :   0,  k:  3.98, theta:  1.73, Payoff:  6.93, Owner:    0,
     Funded: [2.5  2.5  1.03 0.88 0.   0.   0.   0.   0.   0.  ]
    
    Project id:    2, Index :   1,  k:  2.63, theta:  1.94, Payoff:  1.61, Owner:    1,
     Funded: False
    
    Project id:    3, Index :   2,  k:  2.77, theta:  0.53, Payoff:  1.04, Owner:    2,
     Funded: [0.   0.   1.47 0.   0.   0.   0.   0.   0.   0.  ]
    
    Project id:    4, Index :   3,  k:  2.96, theta:  1.93, Payoff:  2.68, Owner:    3,
     Funded: False
    
    Project id:    5, Index :   4,  k:  2.86, theta:  1.12, Payoff:  1.86, Owner:    4,
     Funded: False
    
    Project id:    6, Index :   5,  k:  1.46, theta:  1.38, Payoff:  4.56, Owner:    5,
     Funded: [0.   0.   0.   0.   0.   2.02 0.   0.   0.   0.  ]
    
    Project id:    7, Index :   6,  k: 0.997, theta:  0.75, Payoff:  0.73, Owner:    6,
     Funded: [0.   0.   0.   0.   0.   0.   0.75 0.   0.   0.  ]
    
    Project id:    8, Index :   7,  k:  1.84, theta:  1.23, Payoff:  2.72, Owner:    7,
     Funded: [0.   0.   0.   0.   0.   0.   0.   2.27 0.   0.  ]
    
    Project id:    9, Index :   8,  k:  2.37, theta:  1.04, Payoff:  0.95, Owner:    8,
     Funded: [0.   0.   0.   0.   0.   0.   0.   0.   2.47 0.  ]
    
    Project id:   10, Index :   9,  k:  3.09, theta:  1.46, Payoff:  5.66, Owner:    9,
     Funded: False
    
    Project id:    1, Index :   0,  Status:   0.0, Cash:   2.5, Projects: [0, 0]
    		Distances: [ 0. 10. 10. 20. 20. 30. 30. 30. 20. 10.]
    Project id:    2, Index :   1,  Status:   0.0, Cash:   2.5, Projects: [1, 0]
    		Distances: [10.  0. 10. 10. 20. 20. 30. 30. 30. 20.]
    Project id:    3, Index :   2,  Status:   0.0, Cash:   2.5, Projects: [2, 2, 0]
    		Distances: [20. 10.  0. 10. 10. 20. 20. 30. 30. 30.]
    Project id:    4, Index :   3,  Status:   0.0, Cash:   2.5, Projects: [3, 0, 9]
    		Distances: [30. 20. 10.  0. 10. 10. 20. 20. 30. 30.]
    Project id:    5, Index :   4,  Status:   0.0, Cash:   5.0, Projects: [4]
    		Distances: [30. 30. 20. 10.  0. 10. 10. 20. 20. 30.]
    Project id:    6, Index :   5,  Status:   0.0, Cash:  2.98, Projects: [5, 5]
    		Distances: [30. 30. 30. 20. 10.  0. 10. 10. 20. 20.]
    Project id:    7, Index :   6,  Status:   0.0, Cash:  4.25, Projects: [6, 6]
    		Distances: [20. 30. 30. 30. 20. 10.  0. 10. 10. 20.]
    Project id:    8, Index :   7,  Status:   0.0, Cash:   2.5, Projects: [7, 7, 9]
    		Distances: [20. 20. 30. 30. 30. 20. 10.  0. 10. 10.]
    Project id:    9, Index :   8,  Status:   0.0, Cash:   2.5, Projects: [8, 8, 9]
    		Distances: [10. 20. 20. 30. 30. 30. 20. 10.  0. 10.]
    Project id:   10, Index :   9,  Status:   0.0, Cash:   2.5, Projects: [9, 9]
    		Distances: [10. 10. 20. 20. 30. 30. 30. 20. 10.  0.]
    
    Process finished with exit code 0

--- merchant.py
+++ merchant.py
@@ -7,7 +7,8 @@ class Merchant:
 	count: int = 0
 	number: int = 10
 	neighbour:int = 30
-	reserve: float 2.5
+	reserve: float = 2.5
+	status_cash_conversion: float = 4.
 
 
 
@@ -33,7 +34,9 @@ class Merchant:
 		self.idx: int = n #slot in the array of merchant objects
 		self.status = Merchant.initial_status
 		self.cash = Merchant.initial_cash
+		self.reserve = Merchant.reserve #What the merchant uses to live on, increases with status but can be used to avoid bankruptcy at the expense of status
 		self.projects = []
+		self.cur_funding = np.zeros(Merchant.number, dtype=np.float16)
 		self.connections = np.ones(Merchant.number, dtype=np.float16)*1000.
 		self.distances = np.zeros(Merchant.number, dtype=np.float16)
 
@@ -115,7 +118,17 @@ class Project:
 		return unfunded
 
 	def Print(project):
-		print(f'Project id: {project.id:4}, Index :{project.idx:4},  k: {project.k:5.3}, theta: {project.theta:5.3}, Payoff: {project.payoff:5.3}, Owner: {project.owner:4}, Funded: {project.funded}')
+		if not project.funded:
+			print(f'Project id: {project.id:4}, Index :{project.idx:4},  k: {project.k:5.3}, theta: {project.theta:5.3}, Payoff: {project.payoff:5.3}, Owner: {project.owner:4}, \n Funded: {project.funded}\n')
+		else:
+			print(f'Project id: {project.id:4}, Index :{project.idx:4},  k: {project.k:5.3}, theta: {project.theta:5.3}, Payoff: {project.payoff:5.3}, Owner: {project.owner:4}, \n Funded: {project.investors}\n')
+
+	def AllocateFunds(merchant, merch_id, cash, project, project_id):
+		merchant.cash = merchant.cash - cash
+		merchant.projects.append(project.idx)
+		merchant.cur_funding[project_id] = cash
+		project.investors[merch_id] = cash
+
 
 
 	def Fund(unfunded_list,projects,merchants):#Take a list of unfunded projects idxs and fund themm from the merchant objects in array merchants
@@ -129,26 +142,55 @@ class Project:
 		ranked_unfunded_projects = []
 		for i in list(reversed(np.argsort(preferences).tolist())): #List of
 			ranked_unfunded_projects.append(unfunded_list[i])
-		print("Ranked")
 		for i in ranked_unfunded_projects:
 			owner=projects[i].owner
 			contacts = merchants[owner].distances
-			#Sum up the cash available for the project
+			funding=np.zeros(Merchant.number)#Store the funding before commiting it
+			owner_cash= merchants[owner].cash -  merchants[owner].reserve
 
-			available_cash = 0.0
-			for j in  np.where(contacts < Merchant.neighbour)[0].to_list();
-				if j == i:
-					available_cash = (merchants[j].cash - Merchant.reserve) + available_cash
+			for j in  np.where(contacts < Merchant.neighbour)[0].tolist():#Loop through contacts
+				funds_needed = projects[i].expectation - owner_cash - np.sum(funding)
+				if funds_needed>0.:
+					if j != owner:#The last investor is to be the owner, who will dip into status to fund the project
+						if len(merchants[j].projects) == 0:#Merchant hasn't yet invested, don't go all in
+							funding[j] = min(max(merchants[j].cash - 2. * merchants[j].reserve, 0),funds_needed)
+						else: #Merchant has invested,
+							own_project_funded = False
+							for proj in merchants[j].projects:# but have they invested their own project
+								if projects[proj].owner == j:
+									own_project_funded=True
+							if own_project_funded:
+								funding[j] = min(max(merchants[j].cash -  merchants[j].reserve, 0),funds_needed)
+							else:
+								funding[j] = min(max(merchants[j].cash - 2. * merchants[j].reserve, 0),funds_needed)
+
+			#Looped over all other investors, now see if the owner can fund
+			if projects[i].expectation - owner_cash - np.sum(funding)>0.: #Need to dip into status
+				funding_short = projects[i].expectation - owner_cash - np.sum(funding)
+				if merchants[owner].status > funding_short/Merchant.status_cash_conversion:
+					merchants[owner].status = merchants[owner].status - funding_short/Merchant.status_cash_conversion
+					owner_cash = owner_cash + funding_short
+
+
+
+			if round(projects[i].expectation - owner_cash - np.sum(funding)) == 0.:  #This project is funded
+				Project.AllocateFunds(merchants[owner], owner, owner_cash, projects[i], i)
+				for j in range(Merchant.number):
+					if funding[j]>0.:
+						if j==owner:
+							print(f'Problem: double allocation?? merchants[{j}], {funding[j]}, projects[{i}]')
+						Project.AllocateFunds(merchants[j], j, funding[j], projects[i], i)
+
+				if round(float(np.sum(projects[i].investors)),2) == round(projects[i].expectation,2):
+					projects[i].funded = True
 				else:
-					available_cash = (merchants[j].cash - 2. * Merchant.reserve) + available_cash
+					print(f'Problem funding project {i}: Sum of investments = {round(float(np.sum(projects[i].investors)),2)}, cost = {round(projects[i].expectation,2)}')
+					for i in range(Project.number):
+						Project.Print(projects[i])
+					for i in range(Merchant.number):
+						Merchant.Print(merchants[i])
 
-			if available_cash > projects[i].expectation: #Should fund this
-				#Self-fund
-				requirement = projects[i].expectation - (merchants[owner].cash - Merchant.reserve)
-				merchants[owner].cash =  Merchant.reserve
-				projects[i].investors[owner] = projects[i].expectation
-				#Use closest neighbours first,
-				for j in np.argsort(contacts)[1:].to_list():
+
 
 
 
--- sim.py
+++ sim.py
@@ -31,13 +31,15 @@ for i in range(Project.number):
 	k = (np.random.beta(2, 2) * economy_size ) + merchants[merch_id].status  # This will need developing to account for growth etc
 
 	projects[i] = Project(i, k,thetas[i],merch_id)
+	Project.Print(projects[i])
 	merchants[merch_id].projects.append(i)
 
 
-	if (merchants[merch_id].cash - Merchant.reserve) > projects[i].expectation:
-		merchants[merch_id].cash = (merchants[merch_id].cash - Merchant.reserve) - projects[i].expectation
-		projects[i].investors[merch_id] = projects[i].expectation
+	if (merchants[merch_id].cash - merchants[merch_id].reserve) > projects[i].expectation:
+		Project.AllocateFunds(merchants[merch_id], merch_id, projects[i].expectation, projects[i], i)
+		print(f'Project {i} self funded by {merch_id}')
 		projects[i].funded = True
+
 
 	merch_id += 1
 
@@ -47,6 +49,9 @@ unfunded_list = Project.Unfunded(projects)
 Project.Fund(unfunded_list,projects,merchants )
 
 
+#print(f'\n\nOutput')
+#for i in range(Project.number):
+#	Project.Print(projects[i])
 
 
 
